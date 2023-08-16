c5d66ea812eab81fe2c45bff4fb427ffe2663e43
Hasan6l <141099589+Hasan6l@users.noreply.github.com>
2023-08-17 02:10:17 +0600

    Add files via upload

---
+++ even-or-odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+
+{
+    int x;
+    printf("Enter a Number :\n");
+    scanf("%d",&x);
+    if(x%2==0){
+        printf("\nEven");
+    }
+    else{
+        printf("\nOdd");
+    }
+}
