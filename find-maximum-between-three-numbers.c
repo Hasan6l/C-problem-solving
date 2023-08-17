1bfaca65e02823f29990085b849989b5c1625f15
Hasan6l <141099589+Hasan6l@users.noreply.github.com>
2023-08-18 00:41:22 +0600

    Add files via upload

---
+++ find-maximum-between-three-numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+
+    printf("Enter value of three numbers one by one:\n");
+    scanf("%d %d %d",&x,&y,&z);
+
+    if(x>y && x>y){
+    printf("Maximum value =%d",x);
+    }
+    else if(y>x && y>z){
+    printf("Maximum value =%d",y);
+    }
+    else{
+            printf("Maximum value =%d",z);
+    }
+    return 0;
+}
+
